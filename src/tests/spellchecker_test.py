59dd22fbec13b194942d8518262a1410fd9ca865
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-11-08 13:40:41 +0200

    more wordlist tests

--- src/tests/spellchecker_test.py
+++ src/tests/spellchecker_test.py
@@ -7,3 +7,9 @@ class TestSpellchecker(unittest.TestCase):
 
     def test_word_list_exists(self):
         self.assertIsNotNone(self.dictionary.words)
+
+    def test_word_list_real_word_returns_true(self):
+        self.assertTrue(self.dictionary.look_up_word("hello"))
+
+    def test_word_list_fake_word_returns_false(self):
+        self.assertFalse(self.dictionary.look_up_word("asdf"))
