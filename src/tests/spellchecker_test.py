df8d35e942fa3c2b94cbe2c08535f2d6ff9a7491
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-12-21 11:16:04 +0200

    final unit testing

--- src/tests/spellchecker_test.py
+++ src/tests/spellchecker_test.py
@@ -1,6 +1,5 @@
 import unittest
 from spell_checker import SpellChecker
-from word_list import WordList
 from tests.trie_stub import TrieStub
 
 
@@ -15,8 +14,41 @@ class TestSpellChecker(unittest.TestCase):
         checker._get_incorrect_words()
         return checker
 
-    def test_get_word_at(self):
-        checker=self.create_checker("two words")
-        self.assertEqual("two", checker.get_word_at((1, 3)))
+    def test_give_corrections(self):
+        misspelled_data="cat e pars tins"
+        checker=self.create_checker(misspelled_data)
+        suggestions=[s for i, s in checker.get_suggestions().items()]
+        self.assertEqual(suggestions, ["rat", "a", "a", "in"])
+
+    def test_get_word(self):
+        data="one two three"
+        checker=self.create_checker(data)
+        result=checker.get_word_at((1, 3))
+        self.assertEqual(result, "one")
+        result=checker.get_word_at((5, 3))
+        self.assertEqual(result, "two")
+        result=checker.get_word_at((9, 5))
+        self.assertEqual(result, "three")
+
+    def test_get_text_after_correcting(self):
+        misspelled_data="cat"
+        checker=self.create_checker(misspelled_data)
+        suggestion=[(i,s) for i,s in checker.get_suggestions().items()][0]
+        checker.correct(suggestion[0], suggestion[1])
+        self.assertEqual("rat", checker.get_text())
+
+    def test_word_stays_capitalized(self):
+        misspelled_data="Cat"
+        checker=self.create_checker(misspelled_data)
+        suggestion=[s for i,s in checker.get_suggestions().items()][0]
+        self.assertEqual("Rat", suggestion)
+
+    def test_input_with_no_letters(self):
+        data="123!"
+        checker=self.create_checker(data)
+        self.assertEqual({}, checker._incorrect_words)
+
+
+
 
 
